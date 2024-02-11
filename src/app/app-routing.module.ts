0727983b9d97752c5a46a3caa7490a522b1346fa
Joeway Banh <joewaybanh@hotmail.com>
2024-02-11 17:34:12 +0100

    add routing and home-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PokemonDataComponent } from './components/pokemon-data/pokemon-data.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: 'poke-data', component: PokemonDataComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { PokemonDataComponent } from './components/pokemon-data/pokemon-data.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: 'poke-data', component: PokemonDataComponent },
+];
 
 @NgModule({
-  declarations: [AppComponent, PokemonDataComponent, NavbarComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    PokemonDataComponent,
+    NavbarComponent,
+    HomePageComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+  ],
   providers: [PokemonDataComponent],
   bootstrap: [AppComponent],
+  exports: [RouterModule],
 })
 export class AppModule {}
