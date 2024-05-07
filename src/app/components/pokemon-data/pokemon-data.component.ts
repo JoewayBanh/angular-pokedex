92aa85a8299909c84dbdbcecabeaeb90a56f241d
Joeway Banh <joewaybanh@hotmail.com>
2024-05-07 21:49:24 +0200

    add styling to navbar and start styling for pokemon types

--- src/app/components/pokemon-data/pokemon-data.component.ts
+++ src/app/components/pokemon-data/pokemon-data.component.ts
@@ -12,6 +12,27 @@ export class PokemonDataComponent implements OnInit {
   pokemonName: string = '';
   pokemonId: number = 0;
   favPokemonId: number = 0;
+
+  pokemonTypes = [
+    { type: 'normal', class: 'normal' },
+    { type: 'fire', class: 'fire' },
+    { type: 'water', class: 'water' },
+    { type: 'electric', class: 'electric' },
+    { type: 'grass', class: 'grass' },
+    { type: 'ice', class: 'ice' },
+    { type: 'fighting', class: 'fighting' },
+    { type: 'poison', class: 'poison' },
+    { type: 'ground', class: 'ground' },
+    { type: 'flying', class: 'flying' },
+    { type: 'psychic', class: 'psychic' },
+    { type: 'bug', class: 'bug' },
+    { type: 'rock', class: 'rock' },
+    { type: 'ghost', class: 'ghost' },
+    { type: 'dragon', class: 'dragon' },
+    { type: 'dark', class: 'dark' },
+    { type: 'steel', class: 'steel' },
+    { type: 'fairy', class: 'fairy' },
+  ];
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
@@ -25,32 +46,32 @@ export class PokemonDataComponent implements OnInit {
     this.http
       .get(`https://pokeapi.co/api/v2/pokemon/${this.pokemonId}/`)
       .subscribe((data) => {
-        console.log(data);
+        // console.log(data);
         this.pokemonData = data;
       });
-    console.log(this.pokemonId);
+    // console.log(this.pokemonId);
   }
 
   getFavPokemonData() {
     this.http
       .get(`https://pokeapi.co/api/v2/pokemon/${this.favPokemonId}/`)
       .subscribe((data) => {
-        console.log(data);
+        // console.log(data);
         this.favPokemonData = data;
       });
-    console.log(this.pokemonId);
+    // console.log(this.pokemonId);
   }
 
   getRandomPokemon() {
     this.pokemonId = Math.floor(Math.random() * 1025);
     this.getPokemonData();
-    console.log(this.pokemonId);
-    console.log(this.pokemonData);
+    // console.log(this.pokemonId);
+    // console.log(this.pokemonData);
   }
   getRandomFavPokemon() {
     this.favPokemonId = Math.floor(Math.random() * 150);
     this.getFavPokemonData();
-    console.log(this.favPokemonId);
+    // console.log(this.favPokemonId);
   }
 
   capitalizeName = (pokemonData: any): string => {
@@ -61,5 +82,12 @@ export class PokemonDataComponent implements OnInit {
 
   capitalizeFirstLetter(pokemonData: any): string {
     return pokemonData?.charAt(0).toUpperCase() + pokemonData?.slice(1);
+  }
+  getOptionClass(content: string): string {
+    const option = this.pokemonData.types.find(
+      (opt: any) => opt.name === content
+    );
+    console.log(this.pokemonData.types);
+    return option ? option.class : '';
   }
 }
